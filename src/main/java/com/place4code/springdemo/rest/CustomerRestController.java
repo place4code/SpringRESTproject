a62e4227ae3adc99e28726fd0549ab7e407152fe
place4code <place4code@gmail.com>
2019-04-11 15:50:28 +0200

    add customer POST

--- src/main/java/com/place4code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/place4code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,14 @@ public class CustomerRestController {
 		return tempCustomer;
 	}
 	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		
+		customer.setId(0); // 0 = save/insert new item
+		customerService.saveCustomer(customer);
+		
+		return customer;
+	}
 	
 
 }
