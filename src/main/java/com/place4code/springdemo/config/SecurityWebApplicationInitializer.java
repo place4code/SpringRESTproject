104e81135135822c7a6f6e816017964fa48823bb
place4code <place4code@gmail.com>
2019-04-12 10:18:51 +0200

    adding basic Spring Security

---
+++ src/main/java/com/place4code/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.place4code.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
