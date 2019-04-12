ffa1fc0e17c850cfa40ffcc3b19edb645e8cb840
place4code <place4code@gmail.com>
2019-04-12 10:04:15 +0200

    complete crud rest

--- src/main/java/com/place4code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/place4code/springdemo/rest/CustomerRestController.java
@@ -3,9 +3,11 @@ package com.place4code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +46,23 @@ public class CustomerRestController {
 		return customer;
 	}
 	
+	@PutMapping("/customers")
+	public Customer putCustomer(@RequestBody Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		return customer;
+		
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		
+		Customer temp = customerService.getCustomer(customerId);
+		
+		if (temp == null) throw new CustomerNotFoundException("Not found with id: " + customerId);
+		
+		customerService.deleteCustomer(customerId);
+		return "Deleted [id: " + customerId + " ]";
+	}
 
 }
